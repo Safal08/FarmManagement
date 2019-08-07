9962d75102f47f8101470fbebece18b92d59118f
Safal08 <safalmahat5@gmail.com>
2019-08-07 22:03:20 +0545

    First Commit

--- app/src/main/java/com/example/naruto/farmmanagement/Choose.java
+++ app/src/main/java/com/example/naruto/farmmanagement/Choose.java
@@ -5,14 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class Choose extends AppCompatActivity {
 
     public Button farmer_btn , cus_btn;
+    ViewFlipper vflipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+
+        int images[]={R.drawable.s1, R.drawable.s2, R.drawable.s3};
+
+        vflipper=findViewById(R.id.flipper);
+
+        for(int image: images)
+        {
+            flipperImages(image);
+        }
 
         farmer_btn =(Button) findViewById(R.id.fbt);
         farmer_btn.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +52,18 @@ public class Choose extends AppCompatActivity {
     {
         Intent cus = new Intent(this, customer.class);
         startActivity(cus);
+    }
+
+
+
+    public void flipperImages(int image)
+    {
+        ImageView imgView = new ImageView(this);
+        imgView.setBackgroundResource(image);
+        vflipper.addView(imgView);
+        vflipper.setFlipInterval(4000);
+        vflipper.setAutoStart(true);
+        vflipper.setInAnimation(this, android.R.anim.slide_in_left);
+        vflipper.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 }
