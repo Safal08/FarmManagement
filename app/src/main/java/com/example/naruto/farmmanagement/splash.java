b3d37b2e71380d806ec9be577dead9ef82afd2a9
Safal08 <safalmahat5@gmail.com>
2019-08-08 11:36:46 +0545

    Splash

---
+++ app/src/main/java/com/example/naruto/farmmanagement/splash.java
@@ -0,0 +1,39 @@
+package com.example.naruto.farmmanagement;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageV = findViewById(R.id.imageview);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+        imageV.startAnimation(animation);
+
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+
+                try {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(),Choose.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        timer.start();
+    }
+}
